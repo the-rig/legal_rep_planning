56fe9770a2e276d9df45fa9ba22a4bde9e5bb8e4
Joe Mienko <mienkoja@gmail.com>
2018-08-28 02:53:52 -0700

    adding to legal_rep_plan for IRB - sampling information.

--- global.r
+++ global.r
@@ -79,13 +79,13 @@ build_point_estimates_lewis <- function(stop_date = 20180901, caseload = 60, sal
   historical_survival <- historical_survival_all[historical_survival_locations]
   
   entries_actual <- dplyr::filter(all_data$dat_stock_and_flow.feather
-                           ,tx_county == "Lewis"
+                           ,tx_county_mod == "Lewis"
                            ,month >= lubridate::ymd(20170901)) %>%
     mutate(month = lubridate::as_date(month, tz = "UTC")) %>%
     select(month, total_inflow_of_placements)
   
   exits_actual <- dplyr::filter(all_data$dat_stock_and_flow.feather
-                         ,tx_county == "Lewis"
+                         ,tx_county_mod == "Lewis"
                          ,month >= lubridate::ymd(20170901)) %>%
     mutate(month = lubridate::as_date(month, tz = "UTC")) %>%
     select(month, total_outflow_of_placements)
@@ -158,13 +158,13 @@ build_point_estimates_grant <- function(stop_date = 20180901, caseload = 60, sal
   historical_survival <- historical_survival_all[historical_survival_locations]
   
   entries_actual <- dplyr::filter(all_data$dat_stock_and_flow.feather
-                           ,tx_county == "Grant"
+                           ,tx_county_mod == "Grant"
                            ,month >= lubridate::ymd(20170901)) %>%
     mutate(month = lubridate::as_date(month, tz = "UTC")) %>%
     select(month, total_inflow_of_placements)
   
   exits_actual <- dplyr::filter(all_data$dat_stock_and_flow.feather
-                         ,tx_county == "Grant"
+                         ,tx_county_mod == "Grant"
                          ,month >= lubridate::ymd(20170901)) %>%
     mutate(month = lubridate::as_date(month, tz = "UTC")) %>%
     select(month, total_outflow_of_placements)
@@ -184,6 +184,163 @@ build_point_estimates_grant <- function(stop_date = 20180901, caseload = 60, sal
   entries <- bind_rows(
     entries_actual
     ,as_data_frame(all_data$deseasonal_entries_grant_fcast.feather) %>%
+      mutate(month = lubridate::as_date(month_name_pred)
+             ,total_inflow_of_placements = `Point Forecast`
+             ,total_inflow_of_placements = ifelse(month > lubridate::ymd(stop_date)
+                                                  ,0
+                                                  ,total_inflow_of_placements)) %>%
+      select(month, total_inflow_of_placements)
+  ) %>%
+    arrange(month) %>%
+    .$total_inflow_of_placements
+  
+  expected_study_exits <- (1-historical_survival)*exits
+  
+  data_frame(months_into_pilot
+             ,month_name
+             ,historical_survival
+             ,exits 
+             ,entries
+             ,expected_study_exits)  %>%
+    mutate(entries = ifelse(is.na(entries), 0, entries)
+           ,entries_minus_exits = entries - expected_study_exits
+           ,study_stock = ifelse(cumsum(entries_minus_exits) < 0
+                                 ,0
+                                 ,cumsum(entries_minus_exits))
+           ,attorneys_needed = ifelse(month_name < lubridate::today()
+                                      ,ceiling(study_stock/60)
+                                      ,ceiling(study_stock/caseload)
+           )
+           ,attorney_cost = ifelse(month_name < lubridate::today()
+                                   ,attorneys_needed*(120000/12)
+                                   ,attorneys_needed*(salary/12)
+           )
+           ,cumsum_atty_cost = cumsum(attorney_cost)
+    )
+  
+}
+
+
+build_point_estimates_douglas <- function(stop_date = 20180901, caseload = 60, salary = 120000) {
+  
+  historical_survival_locations <- as.numeric(lapply(X = 1:70
+                                                     ,FUN = vector_search
+                                                     ,times = all_data %>%
+                                                       .$historical_surv_douglas.feather %>%
+                                                       .$time)
+  )
+  
+  historical_survival_all <- all_data %>%
+    .$historical_surv_douglas.feather %>%
+    .$surv
+  
+  historical_survival <- historical_survival_all[historical_survival_locations]
+  
+  entries_actual <- dplyr::filter(all_data$dat_stock_and_flow.feather
+                                  ,tx_county_mod == "Grant"
+                                  ,month >= lubridate::ymd(20170901)) %>%
+    mutate(month = lubridate::as_date(month, tz = "UTC")) %>%
+    select(month, total_inflow_of_placements)
+  
+  exits_actual <- dplyr::filter(all_data$dat_stock_and_flow.feather
+                                ,tx_county_mod == "Grant"
+                                ,month >= lubridate::ymd(20170901)) %>%
+    mutate(month = lubridate::as_date(month, tz = "UTC")) %>%
+    select(month, total_outflow_of_placements)
+  
+  #vector of all exits, predicted and actual
+  exits <- bind_rows(
+    exits_actual
+    ,as_data_frame(all_data$deseasonal_exits_douglas_fcast.feather) %>%
+      mutate(month = lubridate::as_date(month_name_pred)
+             ,total_outflow_of_placements = `Point Forecast`) %>%
+      select(month, total_outflow_of_placements)
+  ) %>%
+    arrange(month) %>%
+    .$total_outflow_of_placements
+  
+  #vector of all entries, predicted and actual, as filtered by stop-date
+  entries <- bind_rows(
+    entries_actual
+    ,as_data_frame(all_data$deseasonal_entries_douglas_fcast.feather) %>%
+      mutate(month = lubridate::as_date(month_name_pred)
+             ,total_inflow_of_placements = `Point Forecast`
+             ,total_inflow_of_placements = ifelse(month > lubridate::ymd(stop_date)
+                                                  ,0
+                                                  ,total_inflow_of_placements)) %>%
+      select(month, total_inflow_of_placements)
+  ) %>%
+    arrange(month) %>%
+    .$total_inflow_of_placements
+  
+  expected_study_exits <- (1-historical_survival)*exits
+  
+  data_frame(months_into_pilot
+             ,month_name
+             ,historical_survival
+             ,exits 
+             ,entries
+             ,expected_study_exits)  %>%
+    mutate(entries = ifelse(is.na(entries), 0, entries)
+           ,entries_minus_exits = entries - expected_study_exits
+           ,study_stock = ifelse(cumsum(entries_minus_exits) < 0
+                                 ,0
+                                 ,cumsum(entries_minus_exits))
+           ,attorneys_needed = ifelse(month_name < lubridate::today()
+                                      ,ceiling(study_stock/60)
+                                      ,ceiling(study_stock/caseload)
+           )
+           ,attorney_cost = ifelse(month_name < lubridate::today()
+                                   ,attorneys_needed*(120000/12)
+                                   ,attorneys_needed*(salary/12)
+           )
+           ,cumsum_atty_cost = cumsum(attorney_cost)
+    )
+  
+}
+
+build_point_estimates_whatcom <- function(stop_date = 20180901, caseload = 60, salary = 120000) {
+  
+  historical_survival_locations <- as.numeric(lapply(X = 1:70
+                                                     ,FUN = vector_search
+                                                     ,times = all_data %>%
+                                                       .$historical_surv_whatcom.feather %>%
+                                                       .$time)
+  )
+  
+  historical_survival_all <- all_data %>%
+    .$historical_surv_whatcom.feather %>%
+    .$surv
+  
+  historical_survival <- historical_survival_all[historical_survival_locations]
+  
+  entries_actual <- dplyr::filter(all_data$dat_stock_and_flow.feather
+                                  ,tx_county_mod == "Grant"
+                                  ,month >= lubridate::ymd(20170901)) %>%
+    mutate(month = lubridate::as_date(month, tz = "UTC")) %>%
+    select(month, total_inflow_of_placements)
+  
+  exits_actual <- dplyr::filter(all_data$dat_stock_and_flow.feather
+                                ,tx_county_mod == "Grant"
+                                ,month >= lubridate::ymd(20170901)) %>%
+    mutate(month = lubridate::as_date(month, tz = "UTC")) %>%
+    select(month, total_outflow_of_placements)
+  
+  #vector of all exits, predicted and actual
+  exits <- bind_rows(
+    exits_actual
+    ,as_data_frame(all_data$deseasonal_exits_whatcom_fcast.feather) %>%
+      mutate(month = lubridate::as_date(month_name_pred)
+             ,total_outflow_of_placements = `Point Forecast`) %>%
+      select(month, total_outflow_of_placements)
+  ) %>%
+    arrange(month) %>%
+    .$total_outflow_of_placements
+  
+  #vector of all entries, predicted and actual, as filtered by stop-date
+  entries <- bind_rows(
+    entries_actual
+    ,as_data_frame(all_data$deseasonal_entries_whatcom_fcast.feather) %>%
       mutate(month = lubridate::as_date(month_name_pred)
              ,total_inflow_of_placements = `Point Forecast`
              ,total_inflow_of_placements = ifelse(month > lubridate::ymd(stop_date)
